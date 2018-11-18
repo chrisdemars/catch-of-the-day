eb09b0a41d36e1a4d3490857c8b9ebec89565beb
Chris DeMars <cdemars86@gmail.com>
2018-11-18 00:21:56 -0500

    added some stuff

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
+  goToStore(e) {
+    // 1. Stop the form from submitting
+    e.preventDefault();
+    // 2. Get the text from the input
+    // 3. Change the page  to /store/whatever-they-entered
+  }
   render() {
     return (
-      <form className='store-selector'>
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name"/>
-        <button type='submit'>Visit Store -></button>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Find store</button>
       </form>
-    )
+    );
   }
 }
 
