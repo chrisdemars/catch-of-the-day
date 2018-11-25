c20e361d8c756e28bee79a94ed998110c961a9d9
Chris DeMars <cdemars86@gmail.com>
2018-11-25 15:29:45 -0500

    stuff

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyCA7zanUSdiAMg9U-GOLB5XGPz4fX7rXZ4',
-  authDomain: 'catch-of-the-day-chris-demars.firebaseapp.com',
-  databaseURL: 'https://catch-of-the-day-chris-demars.firebaseio.com'
+  apiKey: 'AIzaSyBFd1GpcT8ls5GL4ttZ44IXktLrCGPO8xI',
+  authDomain: 'catch-of-the-day-b779a.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-b779a.firebaseio.com'
 });
 
 const base = Rebase.createClass(firebaseApp.database());
